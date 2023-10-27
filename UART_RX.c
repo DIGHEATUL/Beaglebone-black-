dcc0e4655eaf607fd705ba4366328c2dc7bd5889
Atul Dighe <109819207+DIGHEATUL@users.noreply.github.com>
2023-10-27 17:35:26 +0530

    Add files via upload

---
+++ UART_RX.c
@@ -0,0 +1,98 @@
+// C library headers
+#include <stdio.h>
+#include <string.h>
+
+// Linux headers
+#include <fcntl.h> // Contains file controls like O_RDWR
+#include <errno.h> // Error integer and strerror() function
+#include <termios.h> // Contains POSIX terminal control definitions
+#include <unistd.h> // write(), read(), close()
+
+
+
+#define MAX_BUFFER_SIZE 256
+
+
+int main() {
+  // Open the serial port. Change device path as needed (currently set to an standard FTDI USB-UART cable type device)
+  int serial_port = open("/dev/ttyO2", O_RDWR);
+
+  // Create new termios struct, we call it 'tty' for convention
+  struct termios tty;
+
+  // Read in existing settings, and handle any error
+  if(tcgetattr(serial_port, &tty) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+
+  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+  tty.c_cflag |= CS8; // 8 bits per byte (most common)
+  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO; // Disable echo
+  tty.c_lflag &= ~ECHOE; // Disable erasure
+  tty.c_lflag &= ~ECHONL; // Disable new-line echo
+  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+  // tty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
+  // tty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
+
+  tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VMIN] = 0;
+
+  // Set in/out baud rate to be 9600
+  cfsetispeed(&tty, B9600);
+  cfsetospeed(&tty, B9600);
+
+  // Save tty settings, also checking for error
+  if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+
+ // Receive and print messages
+    char rx_buffer[MAX_BUFFER_SIZE];
+    char rx_temp_buffer[MAX_BUFFER_SIZE];
+    int rx_temp_length = 0;
+
+    while (1) {
+        int rx_length = read(serial_port, &rx_temp_buffer, sizeof(rx_temp_buffer));
+
+        if (rx_length < 0) {
+            perror("Error reading from UART");
+            break;
+        } else if (rx_length == 0) {
+            // No data available
+            usleep(10000); // Small delay to prevent busy-waiting
+        } else {
+            // Copy received data to the buffer
+            memcpy(rx_buffer + rx_temp_length, rx_temp_buffer, rx_length);
+            rx_temp_length += rx_length;
+
+            // Check if a newline character is received
+            if (strchr(rx_buffer, '\n') != NULL) {
+                // Null-terminate the received string
+                rx_buffer[rx_temp_length] = '\0';
+
+                // Print the received string
+                printf("Received: %s", rx_buffer);
+
+                // Reset the temporary buffer
+                memset(rx_buffer, 0, sizeof(rx_buffer));
+                rx_temp_length = 0;
+            }
+        }
+    }
+  
+  close(serial_port);
+  return 0; // success
+};
